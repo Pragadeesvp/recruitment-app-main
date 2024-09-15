2ea89c1c1133cf62ca263259f9e626d11e309694
Pragadeesvp <vishalpragadeesvp@gmail>
2024-09-15 13:08:55 +0530

    first commit

---
+++ server/src/index.js
@@ -0,0 +1,10 @@
+const app = require('./app');
+
+const HOST = process.env.HOST ||'0.0.0.0'; // Set the default host to '0.0.0.0'
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, HOST, () => {
+  console.log(`Running on http://${HOST}:${PORT}`);
+});
+
+
